5138689e14602b6aa921153d99152f98c9f26d33
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-11 20:17:11 -0400

    animated juice

--- dev/js/scrollTrigger/juice.js
+++ dev/js/scrollTrigger/juice.js
@@ -1,13 +1,14 @@
 var juiceTimeline = gsap.timeline();
 juiceTimeline.to("#juice",{duration:5, yPercent: 80})
+            .to("#straw-juice",{duration:1, yPercent: 80})
 
 ScrollTrigger.create({
     trigger: "#right-arm",
     start: "top 70%",
-    end: "bottom -1800px",
+    end: "bottom -1900px",
     animation: juiceTimeline,
     toggleActions: "restart none reverse none",
     scrub: 30,
-    id: "juice"
-    // markers: true
+    id: "juice",
+    markers:  true
 });
