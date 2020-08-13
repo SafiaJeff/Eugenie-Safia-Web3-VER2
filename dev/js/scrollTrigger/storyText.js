752f4b67c5902791db3d343a0638d11a4e713b4b
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-13 15:40:19 -0400

    fixed story margins

--- dev/js/scrollTrigger/storyText.js
+++ dev/js/scrollTrigger/storyText.js
@@ -1,26 +1,26 @@
-// var leftXPercent =5;
-// var RightXPercent = -5;
+var leftXPercent =5;
+var RightXPercent = -5;
 
-// var storyTextTimeline = gsap.timeline();
+var storyTextTimeline = gsap.timeline();
 
-// storyTextTimeline.from("#part1L", {alpha:0, xPercent:leftXPercent})
-//                 .from("#part1R", {alpha:0, xPercent:RightXPercent})
-//                 .from("#part2L", {alpha:0, xPercent:leftXPercent})
-//                 .from("#part2R", {alpha:0, xPercent:RightXPercent})
-//                 .from("#part3L", {alpha:0, xPercent:leftXPercent})
-//                 .from("#part3R", {alpha:0, xPercent:RightXPercent})
-//                 .from("#part4L", {alpha:0, xPercent:leftXPercent})
-
-
+storyTextTimeline.from("#part1L", {alpha:0, xPercent:leftXPercent})
+                .from("#part1R", {alpha:0, xPercent:RightXPercent})
+                .from("#part2L", {alpha:0, xPercent:leftXPercent})
+                .from("#part2R", {alpha:0, xPercent:RightXPercent})
+                .from("#part3L", {alpha:0, xPercent:leftXPercent})
+                .from("#part3R", {alpha:0, xPercent:RightXPercent})
+                .from("#part4L", {alpha:0, xPercent:leftXPercent})
 
 
-// ScrollTrigger.create({
-//     trigger: "#story-text",
-//     start: "top 60%",
-//     end: "bottom 140%",
-//     animation: storyTextTimeline,
-//     toggleActions: "restart pause play stop",
-//     scrub: 1,
-//     // markers: true,
-//     id:"story-text"
-// });
+
+
+ScrollTrigger.create({
+    trigger: "#story-text",
+    start: "top 60%",
+    end: "bottom 140%",
+    animation: storyTextTimeline,
+    toggleActions: "restart pause play stop",
+    scrub: 1,
+    // markers: true,
+    id:"story-text"
+});
